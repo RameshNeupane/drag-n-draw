8e2926953796130d810dbb1fd946b191a4fb2654
RameshNeupane <neupaneramesh555@gmail.com>
2022-06-30 23:32:42 +0545

    remove comment

--- frontend/js/canvas.js
+++ frontend/js/canvas.js
@@ -6,10 +6,6 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 const ctx = canvas.getContext("2d");
 
-// canvas.addEventListener("mousemove", (event) => {
-//   console.log(event.clientX, event.clientY);
-// });
-
 const clearCanvas = () => {
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 };
